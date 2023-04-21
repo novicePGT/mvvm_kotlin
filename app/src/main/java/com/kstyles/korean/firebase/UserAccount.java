858f9a484135532aca0ba70c5fadc395825a6886
novicePGT <dymnam@naver.com>
2023-04-22 02:13:42 +0900

    [feat]: 사용자 계정을 위한 클래스 추가

---
+++ app/src/main/java/com/kstyles/korean/firebase/UserAccount.java
@@ -0,0 +1,70 @@
+package com.kstyles.korean.firebase;
+
+public class UserAccount {
+
+    private int seq;
+    private String userEmail;
+    private String userPassword;
+    private String userName;
+    private String date;
+
+    private String idToken;
+
+    public UserAccount(String userEmail, String userPassword, String userName) {
+        this.userEmail = userEmail;
+        this.userPassword = userPassword;
+        this.userName = userName;
+    }
+
+    /**
+     * GETTER & SETTER
+     * @return
+     */
+    public int getSeq() {
+        return seq;
+    }
+
+    public void setSeq(int seq) {
+        this.seq = seq;
+    }
+
+    public String getUserEmail() {
+        return userEmail;
+    }
+
+    public void setUserEmail(String userEmail) {
+        this.userEmail = userEmail;
+    }
+
+    public String getUserPassword() {
+        return userPassword;
+    }
+
+    public void setUserPassword(String userPassword) {
+        this.userPassword = userPassword;
+    }
+
+    public String getUserName() {
+        return userName;
+    }
+
+    public void setUserName(String userName) {
+        this.userName = userName;
+    }
+
+    public String getDate() {
+        return date;
+    }
+
+    public void setDate(String date) {
+        this.date = date;
+    }
+
+    public String getIdToken() {
+        return idToken;
+    }
+
+    public void setIdToken(String idToken) {
+        this.idToken = idToken;
+    }
+}

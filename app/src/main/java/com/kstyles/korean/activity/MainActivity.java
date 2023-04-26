8335fd6bea4b1872909705f146deed30341345aa
novicePGT <dymnam@naver.com>
2023-04-26 17:59:39 +0900

    [feat]: fragment 구현 & 예제 페이지 추가( 추후 변경 )

--- app/src/main/java/com/kstyles/korean/activity/MainActivity.java
+++ app/src/main/java/com/kstyles/korean/activity/MainActivity.java
@@ -1,5 +1,6 @@
 package com.kstyles.korean.activity;
 
+import android.app.Activity;
 import android.os.Bundle;
 import android.util.Log;
 import android.view.View;
@@ -7,6 +8,8 @@ import android.view.View;
 import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.fragment.app.Fragment;
+import androidx.fragment.app.FragmentTransaction;
 import androidx.recyclerview.widget.LinearLayoutManager;
 import androidx.recyclerview.widget.RecyclerView;
 
@@ -17,11 +20,17 @@ import com.google.firebase.database.FirebaseDatabase;
 import com.google.firebase.database.ValueEventListener;
 import com.kstyles.korean.adapter.RecyclerAdapter;
 import com.kstyles.korean.databinding.ActivityMainBinding;
+import com.kstyles.korean.fragment.Ex2Fragment;
+import com.kstyles.korean.fragment.Ex3Fragment;
+import com.kstyles.korean.fragment.Ex4Fragment;
+import com.kstyles.korean.fragment.MainFragment;
 import com.kstyles.korean.item.RecyclerItem;
 
 import java.util.ArrayList;
 
 public class MainActivity extends AppCompatActivity {
+
+    private final Fragment[] fragments = {new MainFragment(), new Ex2Fragment(), new Ex3Fragment(), new Ex4Fragment()};
 
     private String TAG = "[MainActivity]";
     private ActivityMainBinding binding;
@@ -89,5 +98,16 @@ public class MainActivity extends AppCompatActivity {
 
         adapter = new RecyclerAdapter(items, this);
         recyclerView.setAdapter(adapter);
+
+        binding.mainBtnHome.setOnClickListener(v -> switchFragment(fragments[0]));
+        binding.mainBtnEx2.setOnClickListener(v -> switchFragment(fragments[1]));
+        binding.mainBtnEx3.setOnClickListener(v -> switchFragment(fragments[2]));
+        binding.mainBtnEx4.setOnClickListener(v -> switchFragment(fragments[3]));
+    }
+
+    private void switchFragment(Fragment fragment) {
+        FragmentTransaction transaction = getSupportFragmentManager().beginTransaction();
+        transaction.replace(binding.mainFrame.getId(), fragment);
+        transaction.commit();
     }
 }
---
+++ app/src/main/java/com/kstyles/korean/fragment/Ex4Fragment.java
@@ -0,0 +1,23 @@
+package com.kstyles.korean.fragment;
+
+import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
+import androidx.fragment.app.Fragment;
+
+import com.kstyles.korean.R;
+
+public class Ex4Fragment extends Fragment {
+
+    public Ex4Fragment() {}
+
+    @Nullable
+    @Override
+    public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
+        return inflater.inflate(R.layout.activity_fragment_example4, container, false);
+    }
+}

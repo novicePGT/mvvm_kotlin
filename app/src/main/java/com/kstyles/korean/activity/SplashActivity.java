822abf84fa6369869891e7b6c3191efdd50a1499
novicePGT <dymnam@naver.com>
2023-04-28 11:59:11 +0900

    [feat]: 스플래쉬 화면 제작

---
+++ app/src/main/java/com/kstyles/korean/activity/SplashActivity.java
@@ -0,0 +1,54 @@
+package com.kstyles.korean.activity;
+
+import android.content.Intent;
+import android.content.SharedPreferences;
+import android.os.Bundle;
+import android.os.Handler;
+import android.util.Log;
+import android.view.View;
+
+import androidx.annotation.Nullable;
+import androidx.appcompat.app.AppCompatActivity;
+
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+import com.kstyles.korean.databinding.ActivitySplashBinding;
+
+public class SplashActivity extends AppCompatActivity {
+
+    private final String TAG = "[SplashActivity]";
+    private ActivitySplashBinding binding;
+    private final int SPLASH_DURATION = 2000;
+
+    @Override
+    protected void onCreate(@Nullable Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+
+        /**
+         * View Binding Setting
+         */
+        binding = ActivitySplashBinding.inflate(getLayoutInflater());
+        View view = binding.getRoot();
+        setContentView(view);
+
+        new Handler().postDelayed(new Runnable() {
+            @Override
+            public void run() {
+                SharedPreferences sharedPreferences = getSharedPreferences("idToken", 0);
+                String idToken = sharedPreferences.getString("idToken", null);
+                if (!idToken.isEmpty()) {
+                    Intent intent = new Intent(SplashActivity.this, MainActivity.class);
+                    startActivity(intent);
+                    finish();
+                    Log.d(TAG, "TOKEN : " + idToken);
+                }
+                if (idToken.isEmpty()) {
+                    Intent intent = new Intent(SplashActivity.this, LoginActivity.class);
+                    startActivity(intent);
+                    finish();
+                    Log.e(TAG, "TOKEN is null");
+                }
+            }
+        }, SPLASH_DURATION);
+    }
+}

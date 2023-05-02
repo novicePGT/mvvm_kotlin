fbfc3eacaa4324e47d33219974366c9604a4d545
novicePGT <dymnam@naver.com>
2023-05-02 17:43:41 +0900

    [feat]: 데이터베이스에서 정보를 가져오기 위한 메서드 추가

--- app/src/main/java/com/kstyles/korean/repository/FirebaseManager.java
+++ app/src/main/java/com/kstyles/korean/repository/FirebaseManager.java
@@ -9,6 +9,7 @@ import com.google.firebase.database.DatabaseError;
 import com.google.firebase.database.DatabaseReference;
 import com.google.firebase.database.FirebaseDatabase;
 import com.google.firebase.database.ValueEventListener;
+import com.kstyles.korean.item.PracticeItem;
 import com.kstyles.korean.item.RecyclerItem;
 
 import java.util.ArrayList;
@@ -19,13 +20,15 @@ public class FirebaseManager {
     private final String TAG = FirebaseManager.class.getSimpleName();
     private FirebaseDatabase database;
     private DatabaseReference reference;
+    private String pathString;
 
-    public FirebaseManager(String path) {
+    public FirebaseManager() {
         database = FirebaseDatabase.getInstance();
-        reference = database.getReference(path);
+        reference = database.getReference();
     }
 
     public void getRecyclerItems(final FirebaseCallback<List<RecyclerItem>> callback) {
+        reference = database.getReference(pathString);
         reference.addListenerForSingleValueEvent(new ValueEventListener() {
             @Override
             public void onDataChange(@NonNull DataSnapshot snapshot) {
@@ -43,5 +46,29 @@ public class FirebaseManager {
                 callback.onFailure(error);
             }
         });
+    }
+
+    public void getPracticeItems(final FirebaseCallback<List<PracticeItem>> callback) {
+        reference = database.getReference("PracticeItem").child(pathString).child("items");
+        reference.addListenerForSingleValueEvent(new ValueEventListener() {
+            @Override
+            public void onDataChange(@NonNull DataSnapshot snapshot) {
+                List<PracticeItem> items = new ArrayList<>();
+                for (DataSnapshot dataSnapshot : snapshot.getChildren()) {
+                    PracticeItem item = dataSnapshot.getValue(PracticeItem.class);
+                    items.add(item);
+                }
+                callback.onSuccess(items);
+            }
+
+            @Override
+            public void onCancelled(@NonNull DatabaseError error) {
+                Log.e(TAG, "onCancelled() 메서드가 호출됨. {}", error.toException());
+            }
+        });
+    }
+
+    public void setPathString(String pathString) {
+        this.pathString = pathString;
     }
 }

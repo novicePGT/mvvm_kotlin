b916a82fc5fffa0a030b2b155c87cc07be78d842
novicePGT <dymnam@naver.com>
2023-05-02 13:49:44 +0900

    [refactor]: 데이터베이스 구조 변경으로 인한 수정

--- app/src/main/java/com/kstyles/korean/fragment/MainFragment.java
+++ app/src/main/java/com/kstyles/korean/fragment/MainFragment.java
@@ -12,28 +12,25 @@ import androidx.fragment.app.Fragment;
 import androidx.recyclerview.widget.LinearLayoutManager;
 import androidx.recyclerview.widget.RecyclerView;
 
-import com.google.firebase.database.DataSnapshot;
 import com.google.firebase.database.DatabaseError;
-import com.google.firebase.database.DatabaseReference;
-import com.google.firebase.database.FirebaseDatabase;
-import com.google.firebase.database.ValueEventListener;
-import com.kstyles.korean.R;
 import com.kstyles.korean.adapter.RecyclerAdapter;
 import com.kstyles.korean.databinding.ActivityFragmentMainBinding;
 import com.kstyles.korean.item.RecyclerItem;
+import com.kstyles.korean.repository.FirebaseCallback;
+import com.kstyles.korean.repository.FirebaseManager;
 
 import java.util.ArrayList;
+import java.util.List;
 
 public class MainFragment extends Fragment {
 
-    private final String TAG = "[MainFragment] :";
+    private final String TAG = "[MainFragment]";
     private ActivityFragmentMainBinding binding;
     private RecyclerView recyclerView;
     private RecyclerView.LayoutManager layoutManager;
-    private FirebaseDatabase database;
-    private DatabaseReference reference;
     private RecyclerView.Adapter adapter;
-    private ArrayList<RecyclerItem> items;
+    private FirebaseManager firebaseManager;
+    public static ArrayList<RecyclerItem> items;
 
     public MainFragment() {
 
@@ -54,42 +51,24 @@ public class MainFragment extends Fragment {
         items = new ArrayList<>();
 
         /**
-         * firebase setting
-         */
-        database = FirebaseDatabase.getInstance();
-        reference = database.getReference("RecyclerItem"); // db table
-
-        /**
          * firebase data 구현 리스너
          */
-        reference.addListenerForSingleValueEvent(new ValueEventListener() {
-            /**
-             * 데이터베이스의 데이터를 받아오는 곳
-             * @param snapshot The current data at the location
-             */
+        firebaseManager = new FirebaseManager("RecyclerItem");
+        firebaseManager.getRecyclerItems(new FirebaseCallback<List<RecyclerItem>>() {
             @Override
-            public void onDataChange(@NonNull DataSnapshot snapshot) {
-                items.clear();
-                for (DataSnapshot dataSnapshot : snapshot.getChildren()) {
-                    RecyclerItem item = dataSnapshot.getValue(RecyclerItem.class);
-                    items.add(item);
-                }
+            public void onSuccess(List<RecyclerItem> result) {
+                items.addAll(result);
                 adapter.notifyDataSetChanged();
             }
 
-            /**
-             * db 가져오는 중 에러 발생 시 실행
-             * @param error A description of the error that occurred
-             */
             @Override
-            public void onCancelled(@NonNull DatabaseError error) {
-                Log.e(TAG, "메인 화면: onCancelled() 메서드가 호출됨. {}", error.toException());
+            public void onFailure(DatabaseError error) {
+                Log.e(TAG, "onFailure() 메서드가 호출됨. {}", error.toException());
             }
         });
 
         adapter = new RecyclerAdapter(items, getContext());
         recyclerView.setAdapter(adapter);
-
 
         return binding.getRoot();
     }

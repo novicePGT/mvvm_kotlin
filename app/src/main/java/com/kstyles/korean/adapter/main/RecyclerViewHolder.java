8656e5788e301f81f6a29301163b6c44c2d6e73a
novicePGT <dymnam@naver.com>
2023-05-24 14:35:25 +0900

    [feat]: 메인 UI 동적으로 수정

--- app/src/main/java/com/kstyles/korean/adapter/main/RecyclerViewHolder.java
+++ app/src/main/java/com/kstyles/korean/adapter/main/RecyclerViewHolder.java
@@ -1,6 +1,7 @@
 package com.kstyles.korean.adapter.main;
 
 import android.content.Context;
+import android.content.SharedPreferences;
 import android.os.Bundle;
 import android.view.View;
 
@@ -30,8 +31,40 @@ public class RecyclerViewHolder extends RecyclerView.ViewHolder implements View.
     }
 
     public void bind(ArrayList<RecyclerItem> items, int position) {
+        String selectLevel = String.format("%s %s", items.get(position).getLevel(), items.get(position).getName()) + "Word";
+
         binding.itemLevel.setText(items.get(position).getLevel());
         binding.itemName.setText(items.get(position).getName());
+
+        // 상단, 하단의 라인 뷰 제거
+        if (position == 0) {
+            binding.recyclerUpLine.setVisibility(View.INVISIBLE);
+        } else if (position == items.size() - 1) {
+            binding.recyclerDownLine.setVisibility(View.INVISIBLE);
+        } else {
+            binding.recyclerUpLine.setVisibility(View.VISIBLE);
+            binding.recyclerDownLine.setVisibility(View.VISIBLE);
+        }
+
+        SharedPreferences sharedPreferences = context.getSharedPreferences("wordCount", 0);
+        int value = sharedPreferences.getInt(selectLevel, 0);
+        SharedPreferences sharedPreferences1 = context.getSharedPreferences("nextPosition", Context.MODE_PRIVATE);
+        int nextPosition = sharedPreferences1.getInt("nextPosition", 0);
+
+        if (position == 0) {
+            binding.recyclerMainToggle.setBackgroundDrawable(context.getDrawable(R.drawable.custom_toggle_unlock_black));
+        }
+        if (value >= 10) {
+            binding.recyclerMainToggle.setBackgroundDrawable(context.getDrawable(R.drawable.custom_toggle_check_black));
+            SharedPreferences.Editor editor = sharedPreferences1.edit();
+            editor.putInt("nextPosition", position + 1);
+            editor.apply();
+        } else {
+            binding.recyclerMainToggle.setBackgroundDrawable(context.getDrawable(R.drawable.custom_toggle_lock_black));
+        }
+        if (position == nextPosition) {
+            binding.recyclerMainToggle.setBackgroundDrawable(context.getDrawable(R.drawable.custom_toggle_unlock_black));
+        }
     }
 
     @Override

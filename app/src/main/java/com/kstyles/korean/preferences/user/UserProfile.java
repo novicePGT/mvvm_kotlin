c82285332314b5e7eec05d213ad2b3b6656904e5
novicePGT <dymnam@naver.com>
2023-06-14 17:29:27 +0900

    [refactor]: User Uid 기반 Shared Preferences 로 user profile 데이터 저장

--- app/src/main/java/com/kstyles/korean/preferences/user/UserProfile.java
+++ app/src/main/java/com/kstyles/korean/preferences/user/UserProfile.java
@@ -3,14 +3,21 @@ package com.kstyles.korean.preferences.user;
 import android.content.Context;
 import android.content.SharedPreferences;
 
+import com.kstyles.korean.repository.FirebaseManager;
+import com.kstyles.korean.repository.user.User;
+
 public class UserProfile {
 
-    public UserProfile() {
+    private String uid;
 
+    public UserProfile() {
+        FirebaseManager firebaseManager = new FirebaseManager();
+        User user = firebaseManager.getUser();
+        uid = user.getUid();
     }
 
     public String getUserProfileImageUrl(Context context) {
-        SharedPreferences sharedPreferences = context.getSharedPreferences("user_profile", Context.MODE_PRIVATE);
+        SharedPreferences sharedPreferences = context.getSharedPreferences(uid, Context.MODE_PRIVATE);
         return sharedPreferences.getString("user_profile", "");
     }
 }

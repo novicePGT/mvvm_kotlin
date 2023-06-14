eeb456793a2fbd57ab8896a4be96ce98eb5d8c75
novicePGT <dymnam@naver.com>
2023-06-14 16:59:30 +0900

    [refactor]: User Uid 기반 Shared Preferences 로 로그인 데이터 저장

--- app/src/main/java/com/kstyles/korean/activity/SplashActivity.java
+++ app/src/main/java/com/kstyles/korean/activity/SplashActivity.java
@@ -3,6 +3,7 @@ package com.kstyles.korean.activity;
 import android.content.SharedPreferences;
 import android.os.Bundle;
 import android.os.Handler;
+import android.util.Log;
 import android.view.View;
 import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatActivity;
@@ -10,10 +11,12 @@ import com.kstyles.korean.R;
 import com.kstyles.korean.databinding.ActivitySplashBinding;
 import com.kstyles.korean.language.LanguageManager;
 import com.kstyles.korean.repository.FirebaseManager;
+import com.kstyles.korean.repository.user.User;
 
 public class SplashActivity extends AppCompatActivity {
 
     private final String TAG = "[SplashActivity]";
+    private String uid;
     private ActivitySplashBinding binding;
     private final int SPLASH_DURATION = 2000;
 
@@ -36,11 +39,16 @@ public class SplashActivity extends AppCompatActivity {
         binding.tvTop.setText(languageManager.getTranslatedString(R.string.tv_top));
         binding.tvTop2.setText(languageManager.getTranslatedString(R.string.tv_top2));
 
+        /**
+         * user & SharedPreferences
+         */
+        FirebaseManager firebaseManager = new FirebaseManager();
+        User user = firebaseManager.getUser();
+        uid = user.getUid();
+        SharedPreferences sharedPreferences = getSharedPreferences(uid, MODE_PRIVATE);
 
-        SharedPreferences idSharedPreferences = getSharedPreferences("userId", MODE_PRIVATE);
-        String userEmail = idSharedPreferences.getString("userId", "0");
-        SharedPreferences passSharedPreferences = getSharedPreferences("userPass", MODE_PRIVATE);
-        String userPass = passSharedPreferences.getString("userPass", "0");
+        String userEmail = sharedPreferences.getString("user_id", "0");
+        String userPass = sharedPreferences.getString("user_pass", "0");
         new Handler().postDelayed(new Runnable() {
             @Override
             public void run() {

ff6d9650aa6ecf4a1408a8f71b0dcded72a7c854
novicePGT <dymnam@naver.com>
2023-06-19 17:06:13 +0900

    [feat]: Item 형식을 규정하기 위한 클래스 추가

---
+++ app/src/main/java/com/kstyles/korean/view/fragment/item/WordItem.java
@@ -0,0 +1,19 @@
+package com.kstyles.korean.view.fragment.item;
+
+public class WordItem {
+
+    private String wordName;
+    private String description;
+
+    public WordItem() {
+
+    }
+
+    public String getWordName() {
+        return wordName;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+}

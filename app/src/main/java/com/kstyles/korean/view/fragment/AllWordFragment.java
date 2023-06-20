d767e17e777c37fd8d3e0cf05563a8cb7ce356e1
novicePGT <dymnam@naver.com>
2023-06-20 17:13:47 +0900

    [feat]: add All Word fragment

---
+++ app/src/main/java/com/kstyles/korean/view/fragment/AllWordFragment.java
@@ -0,0 +1,81 @@
+package com.kstyles.korean.view.fragment;
+
+import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.LinearLayout;
+import android.widget.TextView;
+
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
+import androidx.fragment.app.Fragment;
+import androidx.recyclerview.widget.LinearLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
+
+import com.kstyles.korean.R;
+import com.kstyles.korean.databinding.ActivityFragmentWordBinding;
+import com.kstyles.korean.language.LanguageManager;
+import com.kstyles.korean.repository.WordManager;
+import com.kstyles.korean.view.fragment.adapter.word.AllWordRecyclerAdapter;
+import com.kstyles.korean.view.fragment.bottomView.BottomViewManipulationListener;
+import com.kstyles.korean.view.fragment.item.WordItem;
+
+import java.util.HashMap;
+
+public class AllWordFragment extends Fragment implements BottomViewManipulationListener {
+
+    private final String TAG = "[AllWordFragment]";
+    private ActivityFragmentWordBinding binding;
+    private RecyclerView recyclerView;
+    private RecyclerView.LayoutManager layoutManager;
+    private RecyclerView.Adapter adapter;
+    private HashMap<String, WordItem> wordsMap;
+
+    public AllWordFragment() {
+
+    }
+
+    @Nullable
+    @Override
+    public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
+        binding = ActivityFragmentWordBinding.inflate(inflater, container, false);
+
+        /**
+         * global variable setting
+         */
+        recyclerView = binding.mainRecycler;
+        recyclerView.setHasFixedSize(true);
+        layoutManager = new LinearLayoutManager(getContext());
+        recyclerView.setLayoutManager(layoutManager);
+
+        LanguageManager languageManager = new LanguageManager(getContext());
+        languageManager.setLanguage();
+        binding.tvWord.setText(languageManager.getTranslatedString(R.string.tv_all_word));
+
+        WordManager wordManager = new WordManager(getContext());
+        wordsMap = wordManager.getAllWords();
+
+        adapter = new AllWordRecyclerAdapter(wordsMap, requireContext());
+        recyclerView.setAdapter(adapter);
+
+        showBottomView();
+
+
+
+        return binding.getRoot();
+    }
+
+    @Override
+    public void hideBottomView() {
+
+    }
+
+    @Override
+    public void showBottomView() {
+        TextView textView = (TextView) getActivity().findViewById(R.id.bottom_navigate_shadow);
+        textView.setVisibility(View.VISIBLE);
+        LinearLayout layout = (LinearLayout) getActivity().findViewById(R.id.bottom_navigate_view);
+        layout.setVisibility(View.VISIBLE);
+    }
+}

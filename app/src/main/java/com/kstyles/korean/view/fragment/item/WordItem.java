d7cd8ad2b9874d7f176a25ea3f0771fccc240972
novicePGT <dymnam@naver.com>
2023-06-20 17:18:26 +0900

    [feat]: Add class for specifying word manager

--- app/src/main/java/com/kstyles/korean/view/fragment/item/WordItem.java
+++ app/src/main/java/com/kstyles/korean/view/fragment/item/WordItem.java
@@ -1,19 +1,28 @@
 package com.kstyles.korean.view.fragment.item;
 
+import android.content.Context;
+import android.content.res.Resources;
+
 public class WordItem {
 
     private String wordName;
     private String description;
 
-    public WordItem() {
-
+    public WordItem(String wordName, String description) {
+        this.wordName = wordName;
+        this.description = description;
     }
 
     public String getWordName() {
         return wordName;
     }
 
-    public String getDescription() {
-        return description;
+    public String getDescription(Context context) {
+        Resources resources = context.getResources();
+        int resourceId = resources.getIdentifier(wordName, "string", context.getPackageName());
+        if (resourceId != 0) {
+            return resources.getString(resourceId);
+        }
+        return "";
     }
 }

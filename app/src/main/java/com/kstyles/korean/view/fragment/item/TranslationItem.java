0a45d4e06392fdad698ff53ebb9c19d4dfc414a2
novicePGT <dymnam@naver.com>
2023-07-06 17:09:46 +0900

    [feat]: 번역을 올리기 위한 클래스 추가

---
+++ app/src/main/java/com/kstyles/korean/view/fragment/item/TranslationItem.java
@@ -0,0 +1,64 @@
+package com.kstyles.korean.view.fragment.item;
+
+import android.content.Context;
+import android.content.SharedPreferences;
+
+import java.util.ArrayList;
+
+public class TranslationItem {
+
+    String en = "";
+
+    String de = "";
+    String fr = "";
+    String ja = "";
+    String th = "";
+    String vi = "";
+    public TranslationItem(String translation, Context context) {
+        SharedPreferences sharedPreferences = context.getSharedPreferences("manager", Context.MODE_PRIVATE);
+        int manager = sharedPreferences.getInt("manager", 0);
+
+        if (manager == 0) {
+            this.en = translation;
+        }
+        if (manager == 1) {
+            this.de = translation;
+        }
+        if (manager == 2) {
+            this.fr = translation;
+        }
+        if (manager == 3) {
+            this.ja = translation;
+        }
+        if (manager == 4) {
+            this.th = translation;
+        }
+        if (manager == 5) {
+            this.vi = translation;
+        }
+    }
+
+    public String getEn() {
+        return en;
+    }
+
+    public String getDe() {
+        return de;
+    }
+
+    public String getFr() {
+        return fr;
+    }
+
+    public String getJa() {
+        return ja;
+    }
+
+    public String getTh() {
+        return th;
+    }
+
+    public String getVi() {
+        return vi;
+    }
+}

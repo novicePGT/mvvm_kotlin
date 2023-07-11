29cbd6982c088d6f74f555ebc17ff88bb352a5a3
novicePGT <dymnam@naver.com>
2023-07-11 16:18:37 +0900

    [feat]: 번역 업데이트 기능 추가

--- app/src/main/java/com/kstyles/korean/view/activity/TranslationActivity.java
+++ app/src/main/java/com/kstyles/korean/view/activity/TranslationActivity.java
@@ -1,26 +1,31 @@
 package com.kstyles.korean.view.activity;
 
+import android.content.SharedPreferences;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.ArrayAdapter;
+import android.widget.EditText;
 import android.widget.Spinner;
 
 import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatActivity;
 
-import com.kstyles.korean.R;
+import com.google.firebase.database.DatabaseError;
 import com.kstyles.korean.databinding.ActivityTranslationBinding;
+import com.kstyles.korean.repository.FirebaseCallback;
 import com.kstyles.korean.repository.FirebaseManager;
 import com.kstyles.korean.view.fragment.item.TranslationItem;
 
 import java.util.ArrayList;
-import java.util.HashMap;
+import java.util.TreeMap;
 
 public class TranslationActivity extends AppCompatActivity {
 
     private ActivityTranslationBinding binding;
     private FirebaseManager firebaseManager;
     private Spinner spinner;
+    private TreeMap<String, TranslationItem> allWordItem;
+    private String item;
 
     @Override
     protected void onCreate(@Nullable Bundle savedInstanceState) {
@@ -30,14 +35,111 @@ public class TranslationActivity extends AppCompatActivity {
         View view = binding.getRoot();
         setContentView(view);
 
+        SharedPreferences sharedPreferences = getSharedPreferences("manager", MODE_PRIVATE);
+        int manager = sharedPreferences.getInt("manager", 0);
+
         firebaseManager = new FirebaseManager();
 
-        HashMap<String, TranslationItem> allWordItem = firebaseManager.getAllWordItem();
-        ArrayList<String> wordSet = new ArrayList<>(allWordItem.keySet());
-        ArrayAdapter<String> spinnerAdapter = new ArrayAdapter<>(this, com.airbnb.lottie.R.layout.support_simple_spinner_dropdown_item, wordSet);
-        spinnerAdapter.setDropDownViewResource(com.airbnb.lottie.R.layout.support_simple_spinner_dropdown_item);
+        firebaseManager.getAllWordItem(new FirebaseCallback<TreeMap<String, TranslationItem>>() {
+            @Override
+            public void onSuccess(TreeMap<String, TranslationItem> wordTranslationItems) {
+                allWordItem = wordTranslationItems;
+                ArrayList<String> wordSet = new ArrayList<>(wordTranslationItems.keySet());
+                ArrayAdapter<String> spinnerAdapter = new ArrayAdapter<>(TranslationActivity.this, com.airbnb.lottie.R.layout.support_simple_spinner_dropdown_item, wordSet);
+                spinnerAdapter.setDropDownViewResource(com.airbnb.lottie.R.layout.support_simple_spinner_dropdown_item);
 
-        spinner = binding.translationSpinner;
-        spinner.setAdapter(spinnerAdapter);
+                spinner = binding.translationSpinner;
+                spinner.setAdapter(spinnerAdapter);
+            }
+
+            @Override
+            public void onFailure(DatabaseError error) {
+                // Handle onFailure event if needed
+            }
+        });
+
+        binding.translationFind.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                item = spinner.getSelectedItem().toString();
+                TranslationItem translationItem = allWordItem.get(item);
+                if (manager == 0) {
+                    binding.translationHintEn.setHint("Here is your Enter: En");
+                    disableNonSelectedEditText(binding.translationHintEn);
+                }
+                if (manager == 1) {
+                    binding.translationHintDe.setHint("Here is your Enter: De");
+                    disableNonSelectedEditText(binding.translationHintDe);
+                }
+                if (manager == 2) {
+                    binding.translationHintFr.setHint("Here is your Enter: Fr");
+                    disableNonSelectedEditText(binding.translationHintFr);
+                }
+                if (manager == 3) {
+                    binding.translationHintJa.setHint("Here is your Enter: Ja");
+                    disableNonSelectedEditText(binding.translationHintJa);
+                }
+                if (manager == 4) {
+                    binding.translationHintTh.setHint("Here is your Enter: Th");
+                    disableNonSelectedEditText(binding.translationHintTh);
+                }
+                if (manager == 5) {
+                    binding.translationHintVi.setHint("Here is your Enter: Vi");
+                    disableNonSelectedEditText(binding.translationHintVi);
+                }
+
+                binding.translationHintEn.setHint(translationItem.getEn());
+                binding.translationHintDe.setHint(translationItem.getDe());
+                binding.translationHintFr.setHint(translationItem.getFr());
+                binding.translationHintJa.setHint(translationItem.getJa());
+                binding.translationHintTh.setHint(translationItem.getTh());
+                binding.translationHintVi.setHint(translationItem.getVi());
+            }
+        });
+
+        binding.translationBtnUpdate.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                String translation = "";
+                String language = "";
+                if (manager == 0) {
+                    translation = item + ": " + binding.translationHintEn.getText().toString();
+                    language = "en";
+                }
+                if (manager == 1) {
+                    translation = item + ": " + binding.translationHintDe.getText().toString();
+                    language = "de";
+                }
+                if (manager == 2) {
+                    translation = item + ": " + binding.translationHintFr.getText().toString();
+                    language = "fr";
+                }
+                if (manager == 3) {
+                    translation = item + ": " + binding.translationHintJa.getText().toString();
+                    language = "ja";
+                }
+                if (manager == 4) {
+                    translation = item + ": " + binding.translationHintTh.getText().toString();
+                    language = "th";
+                }
+                if (manager == 5) {
+                    translation = item + ": " + binding.translationHintVi.getText().toString();
+                    language = "vi";
+                }
+
+                firebaseManager.updateTranslation(item, language, translation);
+            }
+        });
+    }
+
+    private void disableNonSelectedEditText(EditText editText) {
+        binding.translationHintEn.setEnabled(false);
+        binding.translationHintDe.setEnabled(false);
+        binding.translationHintFr.setEnabled(false);
+        binding.translationHintJa.setEnabled(false);
+        binding.translationHintTh.setEnabled(false);
+        binding.translationHintVi.setEnabled(false);
+
+        editText.setEnabled(true);
     }
 }

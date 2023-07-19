21236cffcc145ae4311b915343167c106b603f92
novicePGT <dymnam@naver.com>
2023-07-19 17:14:42 +0900

    [feat]: 뷰홀더에서 모델의 뷰 제어

--- app/src/main/java/com/kstyles/korean/view/fragment/adapter/word/AllWordRecyclerViewHolder.java
+++ app/src/main/java/com/kstyles/korean/view/fragment/adapter/word/AllWordRecyclerViewHolder.java
@@ -1,8 +1,13 @@
 package com.kstyles.korean.view.fragment.adapter.word;
 
+import android.content.Context;
+import android.content.SharedPreferences;
+
 import androidx.recyclerview.widget.RecyclerView;
 
 import com.kstyles.korean.databinding.RecyclerItemWordBinding;
+import com.kstyles.korean.repository.FirebaseManager;
+import com.kstyles.korean.repository.user.User;
 import com.kstyles.korean.view.fragment.item.TranslationItem;
 
 import java.util.ArrayList;
@@ -11,10 +16,12 @@ import java.util.TreeMap;
 public class AllWordRecyclerViewHolder extends RecyclerView.ViewHolder {
 
     private RecyclerItemWordBinding binding;
+    private Context context;
 
-    public AllWordRecyclerViewHolder(RecyclerItemWordBinding binding) {
+    public AllWordRecyclerViewHolder(RecyclerItemWordBinding binding, Context context) {
         super(binding.getRoot());
         this.binding = binding;
+        this.context = context;
     }
 
     public void bind(TreeMap<String, TranslationItem> wordsMap, int position) {
@@ -22,7 +29,37 @@ public class AllWordRecyclerViewHolder extends RecyclerView.ViewHolder {
         String key = keys.get(position);
         TranslationItem translationItem = wordsMap.get(key);
 
+        FirebaseManager firebaseManager = new FirebaseManager();
+        User user = firebaseManager.getUser();
+        String uid = user.getUid();
+
+        SharedPreferences sharedPreferences = context.getSharedPreferences(uid, Context.MODE_PRIVATE);
+        String language = sharedPreferences.getString("language", "en");
+
         binding.tvWord.setText(key);
-        binding.tvDescription.setText(translationItem.getEn()); // 왜 null?
+        if (language.equals("en")) {
+            String[] split = translationItem.getEn().split(":");
+            binding.tvDescription.setText(split[1]);
+        }
+        if (language.equals("de")) {
+            String[] split = translationItem.getDe().split(":");
+            binding.tvDescription.setText(split[1]);
+        }
+        if (language.equals("fr")) {
+            String[] split = translationItem.getFr().split(":");
+            binding.tvDescription.setText(split[1]);
+        }
+        if (language.equals("ja")) {
+            String[] split = translationItem.getJa().split(":");
+            binding.tvDescription.setText(split[1]);
+        }
+        if (language.equals("th")) {
+            String[] split = translationItem.getTh().split(":");
+            binding.tvDescription.setText(split[1]);
+        }
+        if (language.equals("vi")) {
+            String[] split = translationItem.getVi().split(":");
+            binding.tvDescription.setText(split[1]);
+        }
     }
 }

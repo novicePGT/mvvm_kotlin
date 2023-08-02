5de24db487356693a392057446a644a77ae57d98
novicePGT <dymnam@naver.com>
2023-08-02 16:41:54 +0900

    [feat]: registerActivity 에 있던 firebase 의존 코드 이동하면서 폐기 코드 삭제

--- app/src/main/java/com/kstyles/korean/view/activity/RegisterActivity.java
+++ app/src/main/java/com/kstyles/korean/view/activity/RegisterActivity.java
@@ -14,13 +14,6 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import com.bumptech.glide.Glide;
 import com.bumptech.glide.load.DecodeFormat;
-import com.google.android.gms.tasks.OnCompleteListener;
-import com.google.android.gms.tasks.Task;
-import com.google.firebase.auth.AuthResult;
-import com.google.firebase.auth.FirebaseAuth;
-import com.google.firebase.auth.FirebaseUser;
-import com.google.firebase.database.DatabaseReference;
-import com.google.firebase.database.FirebaseDatabase;
 import com.kstyles.korean.R;
 import com.kstyles.korean.databinding.ActivityRegisterBinding;
 import com.kstyles.korean.view.ILocalization;
@@ -32,8 +25,7 @@ import com.kstyles.korean.verification.PasswordValidator;
 
 public class RegisterActivity extends AppCompatActivity implements ILocalization {
 
-    private FirebaseAuth firebaseAuth; // 파이어베이스 인증
-    private DatabaseReference reference; // 실시간 데이터 베이스
+    private final FirebaseManager firebaseManager = new FirebaseManager();
     private ActivityRegisterBinding binding;
     private LanguageManager languageManager;
     private String TAG = "[Register]";
@@ -57,7 +49,6 @@ public class RegisterActivity extends AppCompatActivity implements ILocalization
         /**
          * Set Language
          */
-
         languageManager = new LanguageManager(this);
         languageManager.setLanguage();
         translation();
@@ -65,12 +56,6 @@ public class RegisterActivity extends AppCompatActivity implements ILocalization
         String pass_invalid = languageManager.getTranslatedString(R.string.tv_pass_invalid);
         String successMessage = languageManager.getTranslatedString(R.string.toast_success_regist);
         String failedMessage = languageManager.getTranslatedString(R.string.toast_failed_regist);
-
-        /**
-         * firebase setting
-         */
-        firebaseAuth = FirebaseAuth.getInstance();
-        reference = FirebaseDatabase.getInstance().getReference("UserAccount");
 
         /**
          * 정규식을 이용한 이메일 형식 검증 -> 텍스트로 정보를 노출
@@ -96,7 +81,7 @@ public class RegisterActivity extends AppCompatActivity implements ILocalization
         binding.registerUserProfile.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                Intent intent = new Intent(Intent.ACTION_OPEN_DOCUMENT);
+                Intent intent = new Intent(Intent.ACTION_GET_CONTENT);
                 intent.setType("image/*");
                 startActivityForResult(intent, REQUEST_CODE);
             }
@@ -119,7 +104,17 @@ public class RegisterActivity extends AppCompatActivity implements ILocalization
                 if (PasswordValidator.validatePassword(password1, password2) && !binding.registerUserName.equals("") && !binding.registerUserEmail.equals("")) {
                     binding.registerTvPasswordVerification.setText(pass_valid);
                     binding.registerTvPasswordVerification.setTextColor(Color.BLUE);
-                    register(successMessage, failedMessage);
+                    UserAccount userAccount = new UserAccount(userEmail, userPassword, userProfile, successMessage, failedMessage);
+                    firebaseManager.register(userAccount, RegisterActivity.this);
+
+                    if (userAccount.getProfile() != null) {
+                        firebaseManager.uploadUserProfile(RegisterActivity.this, userAccount.getProfile());
+                    }
+
+                    Intent intent = new Intent(RegisterActivity.this, LoginActivity.class);
+                    startActivity(intent);
+                    finish();
+
                 }
                 if (!PasswordValidator.validatePassword(password1, password2)) {
                     binding.registerTvPasswordVerification.setText(pass_invalid);
@@ -130,35 +125,6 @@ public class RegisterActivity extends AppCompatActivity implements ILocalization
                 }
             }
         });
-    }
-
-    private void register(String successMessage, String failedMessage) {
-        firebaseAuth.createUserWithEmailAndPassword(userEmail, userPassword).addOnCompleteListener(RegisterActivity.this, new OnCompleteListener<AuthResult>() {
-            @Override
-            public void onComplete(@NonNull Task task) {
-                if (task.isSuccessful()) {
-                    FirebaseUser currentUser = firebaseAuth.getCurrentUser();
-                    UserAccount userAccount = new UserAccount(userEmail, userPassword, userName, firebaseAuth.getUid());
-                    reference.child("UserAccount").child(currentUser.getUid()).setValue(userAccount);
-
-                    Toast.makeText(RegisterActivity.this, successMessage, Toast.LENGTH_SHORT).show();
-
-                    if (userProfile != null) {
-                        FirebaseManager firebaseManager = new FirebaseManager();
-                        firebaseManager.uploadUserProfile(RegisterActivity.this, userProfile);
-                    }
-
-                    Intent intent = new Intent(RegisterActivity.this, LoginActivity.class);
-                    startActivity(intent);
-                    finish();
-
-                } else {
-                    Toast.makeText(RegisterActivity.this, failedMessage, Toast.LENGTH_SHORT).show();
-                }
-            }
-        });
-
-
     }
 
     @Override

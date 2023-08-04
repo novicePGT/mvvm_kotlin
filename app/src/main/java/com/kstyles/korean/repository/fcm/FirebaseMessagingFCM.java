b74993fbfba22fb84e2da5c7612313d513bcf0b4
novicePGT <dymnam@naver.com>
2023-08-04 16:42:03 +0900

    [feat]: FCM & Notification 추가

--- app/src/main/java/com/kstyles/korean/repository/fcm/FirebaseMessagingFCM.java
+++ app/src/main/java/com/kstyles/korean/repository/fcm/FirebaseMessagingFCM.java
@@ -1,12 +1,22 @@
 package com.kstyles.korean.repository.fcm;
 
+import android.app.NotificationChannel;
+import android.app.NotificationManager;
+import android.app.PendingIntent;
+import android.content.Context;
+import android.content.Intent;
+import android.os.Build;
 import android.util.Log;
+import android.widget.RemoteViews;
 
 import androidx.annotation.NonNull;
+import androidx.core.app.NotificationCompat;
 
 import com.google.firebase.messaging.FirebaseMessaging;
 import com.google.firebase.messaging.FirebaseMessagingService;
 import com.google.firebase.messaging.RemoteMessage;
+import com.kstyles.korean.R;
+import com.kstyles.korean.view.activity.MainActivity;
 
 public class FirebaseMessagingFCM extends FirebaseMessagingService  {
 
@@ -25,8 +35,11 @@ public class FirebaseMessagingFCM extends FirebaseMessagingService  {
     }
 
     @Override
-    public void onMessageReceived(@NonNull RemoteMessage message) {
-        super.onMessageReceived(message);
+    public void onMessageReceived(@NonNull RemoteMessage remotemessage) {
+
+        if (remotemessage.getNotification() != null) {
+            generateNotification(remotemessage.getNotification().getTitle(), remotemessage.getNotification().getBody());
+        }
     }
 
     private void getToken() {
@@ -43,5 +56,39 @@ public class FirebaseMessagingFCM extends FirebaseMessagingService  {
     }
 
     private void sendRegistrationToServer(String token) {
+    }
+
+    private void generateNotification(String title, String message) {
+        Intent intent = new Intent(this, MainActivity.class);
+        intent.addFlags(Intent.FLAG_ACTIVITY_CLEAR_TOP);
+
+        PendingIntent pendingIntent = PendingIntent.getActivity(this, 0, intent, PendingIntent.FLAG_ONE_SHOT | PendingIntent.FLAG_IMMUTABLE);
+
+        NotificationCompat.Builder builder = new NotificationCompat.Builder(getApplicationContext(), "notification_channel")
+                .setSmallIcon(R.drawable.icon_logo)
+                .setAutoCancel(true)
+                .setVibrate(new long[]{1000, 1000})
+                .setContentIntent(pendingIntent);
+
+        builder = builder.setContent(getRemoteView(title, message));
+
+        NotificationManager notificationManager = (NotificationManager) getSystemService(Context.NOTIFICATION_SERVICE);
+
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.O) {
+            NotificationChannel notification_channel = new NotificationChannel("notification_channel", "com.kstyles.korean.repository.fcm", NotificationManager.IMPORTANCE_HIGH);
+            notificationManager.createNotificationChannel(notification_channel);
+        }
+
+        notificationManager.notify(0, builder.build());
+    }
+
+    private RemoteViews getRemoteView(String title, String message) {
+        RemoteViews remoteView = new RemoteViews(getApplicationContext().getPackageName(), R.layout.input_notification);
+
+        remoteView.setTextViewText(R.id.noti_title, title);
+        remoteView.setTextViewText(R.id.noti_Message, message);
+        remoteView.setImageViewResource(R.id.noti_logo, R.drawable.icon_logo);
+
+        return remoteView;
     }
 }

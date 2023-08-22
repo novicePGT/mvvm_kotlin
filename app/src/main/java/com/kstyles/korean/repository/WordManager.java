b9a8a89049f46f124e846c5e6ce1ec50ef0004ae
geuntae1 <dymnam@naver.com>
2023-08-22 16:37:19 +0900

    [refactor]: 데이터베이스 변경에 따라 코드 수정

--- app/src/main/java/com/kstyles/korean/repository/WordManager.java
+++ app/src/main/java/com/kstyles/korean/repository/WordManager.java
@@ -1,10 +1,6 @@
 package com.kstyles.korean.repository;
 
-import android.content.Context;
-import android.content.res.Resources;
-
 import com.google.firebase.database.DatabaseError;
-import com.kstyles.korean.R;
 import com.kstyles.korean.view.fragment.item.TranslationItem;
 
 import java.util.TreeMap;
@@ -13,71 +9,56 @@ public class WordManager {
 
     private String TAG = "[WordManager]";
     private TreeMap<String, TranslationItem> wordsMap;
-    private TreeMap<String, TranslationItem> cloneWordsMap;
+    private final TreeMap<String, TranslationItem> cloneWordsMap;
+    private final onDataLoadedListener onDataLoaded;
+    private final FirebaseManager firebaseManager;
 
     public interface onDataLoadedListener {
         void onDataLoaded(TreeMap<String, TranslationItem> cloneWordsMap);
     }
 
-    public WordManager(Context context, onDataLoadedListener loadedListener) {
-        FirebaseManager firebaseManager = new FirebaseManager();
+    public WordManager(onDataLoadedListener loadedListener) {
+        firebaseManager = new FirebaseManager();
         wordsMap = new TreeMap<>();
         cloneWordsMap = new TreeMap<>();
-        firebaseManager.getAllWordItem(new FirebaseCallback<TreeMap<String, TranslationItem>>() {
+        onDataLoaded = loadedListener;
+        loadWords("Beginner");
+    }
+
+    public void loadWords(String level) {
+        cloneWordsMap.clear();
+        getWordByLevel(level, new FirebaseCallback() {
             @Override
-            public void onSuccess(TreeMap<String, TranslationItem> result) {
-                wordsMap = result;
-                loadWordsFromResources(context, loadedListener);
+            public void onSuccess(Object result) {
+                TreeMap<String, TranslationItem> beginner = (TreeMap<String, TranslationItem>) result;
+                cloneWordsMap.putAll(beginner);
+                onDataLoaded.onDataLoaded(cloneWordsMap);
             }
 
             @Override
             public void onFailure(DatabaseError error) {
-
+                // 실패 처리 로직 추가
             }
         });
     }
 
-    private void loadWordsFromResources(Context context, onDataLoadedListener loadedListener) {
-        Resources resources = context.getResources();
-        String[] wordNames = resources.getStringArray(R.array.beginner);
-
-        for (String wordName : wordNames) {
-            cloneWordsMap.put(wordName, wordsMap.get(wordName));
-        }
-        loadedListener.onDataLoaded(cloneWordsMap);
-    }
-
-    public void loadBeginnerWords(Context context) {
-        cloneWordsMap.clear();
-        Resources resources = context.getResources();
-        String[] wordNames = resources.getStringArray(R.array.beginner);
-
-        for (String wordName : wordNames) {
-            cloneWordsMap.put(wordName, wordsMap.get(wordName));
-        }
-    }
-
-    public void loadIntermediateWords(Context context) {
-        cloneWordsMap.clear();
-        Resources resources = context.getResources();
-        String[] wordNames = resources.getStringArray(R.array.intermediate);
-
-        for (String wordName : wordNames) {
-            cloneWordsMap.put(wordName, wordsMap.get(wordName));
-        }
-    }
-
-    public void loadAdvancedWords(Context context) {
-        cloneWordsMap.clear();
-        Resources resources = context.getResources();
-        String[] wordNames = resources.getStringArray(R.array.advanced);
-
-        for (String wordName : wordNames) {
-            cloneWordsMap.put(wordName, wordsMap.get(wordName));
-        }
-    }
-
     public TreeMap<String, TranslationItem> getAllWords() {
         return cloneWordsMap;
+    }
+
+    private void getWordByLevel(String level, final FirebaseCallback callback) {
+        wordsMap.clear();
+        firebaseManager.getWordByLevel(level, new FirebaseCallback() {
+            @Override
+            public void onSuccess(Object result) {
+                wordsMap = (TreeMap<String, TranslationItem>) result;
+                callback.onSuccess(wordsMap);
+            }
+
+            @Override
+            public void onFailure(DatabaseError error) {
+                callback.onFailure(error);
+            }
+        });
     }
 }
